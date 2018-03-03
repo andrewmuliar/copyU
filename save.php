684b57c645976d8bf4a8d28bc81ac7a74c11debf
Andrew Muliar <andrewmuliar7@gmail.com>
2018-03-03 12:15:05 +0200

    upgrd

--- save.php
+++ save.php
@@ -2,19 +2,18 @@
 $post = file_get_contents('php://input',true);
 $ar = json_decode($post); //to array from string
 $domain = $ar[0]; //root from user to explode url
+echo var_export($ar);
 $root = 'http://localhost/php_request/'; //host root for deploy url
-echo var_export($ar, true);
-echo 'Array item = '.$ar[1][0].' end ';
-for($i = 0; $i < count($ar); $i++) 
+for($i = 0; $i < count($ar[1]); $i++) 
 {
- /*if(substr($ar[1][$i], 0, strlen($root)) == $root)
+ if(substr($ar[1][$i], 0, strlen($root)) == $root)
  {
-  $ar[1][$i] = str_replace($root, '', $ar[1][$i]);
-  echo ' Replacing '.$ar[1][$i];
-  //deleting our $root from directory
- }*/
+  $ar[1][$i] = str_replace($root, $domain, $ar[1][$i]);
+  //replace root on domain name
+ }
  //remove from link domain name
- $ar[1][$i] = str_replace('http://localhost/php_request/', $domain, $ar[1][$i]);
+ $no_domain = str_replace($root, $domain, $ar[1][$i]);
+ echo ' Replacing '.$no_domain;
  $no_domain = str_replace($domain, '', $no_domain);
  //get posisiton of last slash for directory 
  $last_slash = strripos($no_domain, '/');
@@ -22,10 +21,11 @@ for($i = 0; $i < count($ar); $i++)
  $directory  = '/xampp/htdocs/php_request/uploads/'.substr_replace($no_domain, '', $last_slash); 
 //directory to safe file
  if(!is_dir($directory)) //if no exist
-  mkdir($directory, 2, true);
+  mkdir($directory, 0777, true);
  else
   echo " dir =  ".$directory;
-  $content = file_get_contents($ar[1][$i], true); //Tut podvog
+  $content = file_get_contents($ar[1][$i], true); 
+  echo "get fontnet from ".$ar[1][$i];
   if($content == false) // If script or somethink cannot upload
   {
    $file_error = fopen('/xampp/htdocs/php_request/uploads/error.txt', 'w');
@@ -34,11 +34,11 @@ for($i = 0; $i < count($ar); $i++)
   }
   else
   {
-   $full_path = $directory.'/';
-   $file_write = fopen($full_path, 'w');
+   $directory .= substr($no_domain,$last_slash);
+   echo ' last dir = '.$directory;
+   $file_write = fopen($directory, 'w');
    fwrite($file_write, $content);
    fclose($file_write);
   }
 } 
- //write files
 ?>
